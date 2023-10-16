3ee8b0c1050b1b0576be7b60fe6534d3a9b7da01
Quien Sabe <qs5779@mail.com>
2023-10-16 09:16:32 -0400

    test: Add pytest_click, update poetry (#76)
    Co-authored-by: Quien Sabe <qs5579@mail.com>

--- tests/test_options.py
+++ tests/test_options.py
@@ -6,53 +6,53 @@ test_opts = {"test": True}
 dbg_opts = {"test": False, "debug": 1, "verbose": 0}
 verbose_opts = {"test": False, "debug": 0, "verbose": 1}
 
-TSTR = "When it stops hurting it will feel better."
+CONST_TEST_STRING = "When it stops hurting it will feel better."
 
 
 def test_options_debug_flag(capfd):
     """Test options debug flag."""
     oopts = Options(dbg_opts)
-    oopts.trace(TSTR)
+    oopts.trace(CONST_TEST_STRING)
     assert oopts.isdebug()
     assert not oopts.isverbose()
     assert not oopts.istest()
-    oopts._debug(TSTR)
+    oopts._debug(CONST_TEST_STRING)
     out, err = capfd.readouterr()
-    assert TSTR in out
+    assert CONST_TEST_STRING in out
 
 
 def test_options_verbose_flag(capfd):
     """Test options verbose flag."""
     oopts = Options(verbose_opts)
-    oopts.trace(TSTR)
+    oopts.trace(CONST_TEST_STRING)
     assert not oopts.isdebug()
     assert oopts.isverbose()
     assert not oopts.istest()
-    oopts._verbose(TSTR)
+    oopts._verbose(CONST_TEST_STRING)
     out, err = capfd.readouterr()
-    assert TSTR in out
+    assert CONST_TEST_STRING in out
 
 
 def test_default_options(capfd):
     """Test options defaults."""
     oopts = Options()
-    oopts.trace(TSTR)
+    oopts.trace(CONST_TEST_STRING)
     out, err = capfd.readouterr()
     assert not oopts.isdebug()
     assert not oopts.isverbose()
     assert not oopts.istest()
-    assert TSTR not in out
+    assert CONST_TEST_STRING not in out
 
 
 def test_options_test_flag(capfd):
     """Test options test flag."""
     oopts = Options(test_opts)
-    oopts.trace(TSTR)
+    oopts.trace(CONST_TEST_STRING)
     out, err = capfd.readouterr()
     assert not oopts.isdebug()
     assert not oopts.isverbose()
     assert oopts.istest()
-    assert TSTR in out
+    assert CONST_TEST_STRING in out
 
 
 def test_options_requires_super_user():
